153a7300fd7a106de358e188ef370798949b23b4
Zhaojin <therealchubacca@gmail.com>
2024-06-23 18:28:59 +0400

    fixed tailwindcss errors

--- cs_nea/tailwind.config.js
+++ cs_nea/tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/**/*.html", 
-    "./node_modules/flowbite/**/*.js",
+      './templates/**/*.html'
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
-};
+  plugins: [],
+}
