476c22de6e7e87dbac4e10121fdef7f4df7f1fbc
Zhaojin <therealchubacca@gmail.com>
2024-07-14 12:30:26 +0400

    created student/teacher login/signup

--- cs_nea/users/models.py
+++ cs_nea/users/models.py
@@ -43,12 +43,6 @@ class User(AbstractBaseUser):
         ],
     )
     
-    USER_TYPE_CHOICES = (
-        ('student', 'Student'),
-        ('teacher', 'Teacher'),
-    )
-    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
-    
     contact_number = PhoneNumberField()
     
     date_of_birth = models.DateField("date of birth")
--- cs_nea/users/urls.py
+++ cs_nea/users/urls.py
@@ -3,8 +3,6 @@ from .views import index, login, signup, logout, student, teacher
 
 urlpatterns = [
     path("", index, name="index"),
-    path("login/", login, name="login"),
-    path("signup/", signup, name="signup"),
     path("logout/", logout, name="logout"),
     path("student/", student, name="student"),
     path("teacher/", teacher, name="teacher"),
