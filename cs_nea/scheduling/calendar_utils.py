c1c8caad225a6513f877fd0fbc62ca2e8c5ec777
Zhaojin <therealchubacca@gmail.com>
2024-07-15 16:31:58 +0400

    calendar iteration 2

--- cs_nea/scheduling/calendar_utils.py
+++ cs_nea/scheduling/calendar_utils.py
@@ -1,8 +1,7 @@
-# scheduling/calendar_utils.py
-
 from calendar import HTMLCalendar
 from datetime import date
 from .models import Lesson, OtherEvent
+from django.utils import timezone
 
 class LessonCalendar(HTMLCalendar):
     def __init__(self, year=None, month=None):
@@ -10,22 +9,34 @@ class LessonCalendar(HTMLCalendar):
         self.month = month
         super(LessonCalendar, self).__init__()
 
-    def formatday(self, day, lessons, other_events):
-        lessons_for_day = lessons.filter(start_datetime__day=day)
-        events_for_day = other_events.filter(start_datetime__day=day)
-        d = ''
-        for lesson in lessons_for_day:
-            d += f'<div class="text-xs p-1 bg-purple-200 rounded mb-1">{lesson.start_datetime.strftime("%H:%M")} - Lesson</div>'
-        for event in events_for_day:
-            d += f'<div class="text-xs p-1 bg-blue-200 rounded mb-1">{event.start_datetime.strftime("%H:%M")} - {event.event_description}</div>'
-        if day != 0:
-            return f"<td class='p-2 border border-gray-200'><span class='date'>{day}</span><div class='events'>{d}</div></td>"
-        return '<td class="p-2 border border-gray-200"></td>'
+    def formatday(self, day, weekday, lessons, other_events):
+        today = timezone.now().date()
+        if day == 0:
+            return '<td class="p-2 border border-gray-200 bg-gray-100"></td>'
+        else:
+            date_obj = date(self.year, self.month, day)
+            classes = ['p-2', 'border', 'border-gray-200']
+            if date_obj == today:
+                classes.append('bg-yellow-100')
+            elif date_obj < today:
+                classes.append('bg-gray-50')
+            
+            lessons_for_day = lessons.filter(start_datetime__date=date_obj)
+            events_for_day = other_events.filter(start_datetime__date=date_obj)
+            
+            d = f"<td class='{' '.join(classes)}'>"
+            d += f"<span class='date'>{day}</span>"
+            for lesson in lessons_for_day:
+                d += f'<div class="text-xs p-1 bg-purple-200 rounded mb-1">{lesson.start_datetime.strftime("%H:%M")} - Lesson</div>'
+            for event in events_for_day:
+                d += f'<div class="text-xs p-1 bg-blue-200 rounded mb-1">{event.start_datetime.strftime("%H:%M")} - {event.event_description}</div>'
+            d += "</td>"
+            return d
 
     def formatweek(self, theweek, lessons, other_events):
         week = ''
         for d, weekday in theweek:
-            week += self.formatday(d, lessons, other_events)
+            week += self.formatday(d, weekday, lessons, other_events)
         return f'<tr>{week}</tr>'
 
     def formatmonth(self, withyear=True):
@@ -37,8 +48,4 @@ class LessonCalendar(HTMLCalendar):
         cal += f'{self.formatweekheader()}\n'
         for week in self.monthdays2calendar(self.year, self.month):
             cal += f'{self.formatweek(week, lessons, other_events)}\n'
-        return cal
-    
-    
-    
-    
+        return cal  
