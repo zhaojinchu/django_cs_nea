6433a7354defac031b92674047ddda8d7dbab666
Zhaojin <therealchubacca@gmail.com>
2024-07-25 15:58:44 +0400

    teacher_schedule_lesson v2

--- cs_nea/scheduling/views.py
+++ cs_nea/scheduling/views.py
@@ -220,7 +220,7 @@ def get_student_schedule(request, student_id):
     for lesson in lessons:
         events.append({
             "id": f"lesson_{lesson.lesson_id}",
-            "title": f"Lesson with {Teacher.objects.filter(teacher_id=lesson.teacher_id)}",
+            "title": f"Lesson with {Teacher.objects.get(teacher_id=lesson.teacher_id).user.get_full_name()}",
             "start": lesson.start_datetime.isoformat(),
             "end": lesson.end_datetime.isoformat(),
             "color": "blue",
